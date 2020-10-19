6443d3534ab8064eef4fedb556c79808c1359627
KozubIgn <ignacykozub@gmail.com>
2020-10-19 13:16:32 +0200

    get rate from api and display

--- src/app/currency-list/currency-list.component.ts
+++ src/app/currency-list/currency-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {CurrencyService} from '../currency.service';
+import {ApiService} from '../services/api.service';
 import {Observable} from "rxjs";
 import {ExchangeRate} from "../models/exchangeRate";
 
@@ -9,17 +9,18 @@ import {ExchangeRate} from "../models/exchangeRate";
   styleUrls: ['./currency-list.component.css']
 })
 export class CurrencyListComponent implements OnInit {
-
+  exchangeRate: ExchangeRate;
   currencyRate: Observable<ExchangeRate>
 
   currencySign: string[] = ['USD', 'JPY', 'BGN', 'CZK', 'DKK', 'GBP', 'HUF', 'PLN', 'RON', 'SEK', 'CHF', 'ISK', 'NOK', 'HRK',
     'RUB', 'TRY', 'AUD', 'BRL', 'CAD', 'CNY', 'HKD', 'IDR', 'ILS', 'INR', 'KRW', 'MXN', 'MYR', 'NZD', 'PHP', 'SGD', 'THB', 'ZAR'];
 
-  constructor(private currencyService: CurrencyService) {
+  constructor(private currencyService: ApiService) {
   }
 
   ngOnInit(): void {
-    this.currencyRate = this.currencyService.getExchangeRate({base: 'EUR', to: 'PLN'});
-    console.log(this.currencyRate);
+   this.currencyService.getExchangeRate({base: 'EUR', to: 'PLN'}).subscribe(data =>
+   {this.exchangeRate = data});
+
   }
 }
