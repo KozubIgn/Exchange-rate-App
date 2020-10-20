81dfc286e270613585d2e8add923340a006a87ce
KozubIgn <ignacykozub@gmail.com>
2020-10-20 21:54:54 +0200

    add getHistoricalRate()

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,12 +5,15 @@ import {Observable} from 'rxjs';
 import {ExchangeRate} from "../models/exchangeRate";
 import {Currency} from '../models/currency';
 import {map} from "rxjs/operators";
+import {Result} from "../models/result";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  dateFrom: '2015-10-15';
+  dateTo: '2020-10-20';
   private url = 'https://api.exchangeratesapi.io';
 
   constructor(private  http: HttpClient) {
@@ -28,5 +31,23 @@ export class ApiService {
           };
         })
       );
+  }
+
+  getHistoricalRate(currencies: Currency) {
+    return this.http.get(`https://api.exchangeratesapi.io/history?start_at=${(this.dateFrom)}&end_at=${(this.dateTo)}&base=${currencies.base}&symbols=${currencies.to}`).pipe(
+      map((data: { rates }) => {
+        const result: Result = {
+          name: currencies.to,
+          value: []
+        };
+        for (let date in data.rates) {
+          if (data.rates.hasOwnProperty(date)) {
+            result.value.push({
+              name: date,
+              rateValue: data.rates[date][currencies.to]
+            });
+          }
+        }
+      }))
   }
 }
