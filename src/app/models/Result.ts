05e08ec3f9cf8804f196b1605cef06504d751335
KozubIgn <ignacykozub@gmail.com>
2020-10-20 20:41:06 +0200

    add models dataPart.ts, Result.ts

---
+++ src/app/models/Result.ts
@@ -0,0 +1,6 @@
+import {ChartDataPart} from "./dataPart";
+
+export interface Result {
+   name: string;
+   value: ChartDataPart [];
+}
---
+++ src/app/models/dataPart.ts
@@ -0,0 +1,5 @@
+export interface ChartDataPart {
+  date: string;
+  rateValue: number;
+
+}
