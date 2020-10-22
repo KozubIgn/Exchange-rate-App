93d91569499961861f9dfa7a561ed30c9689393b
KozubIgn <ignacykozub@gmail.com>
2020-10-22 11:34:14 +0200

    add getHistoricalRate();

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,15 +5,14 @@ import {Observable} from 'rxjs';
 import {ExchangeRate} from "../models/exchangeRate";
 import {Currency} from '../models/currency';
 import {map} from "rxjs/operators";
-import {Result} from "../models/result";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  dateFrom: '2015-10-15';
-  dateTo: '2020-10-20';
+  private dateFrom = '2015-10-15';
+  private dateTo = '2020-10-20';
   private url = 'https://api.exchangeratesapi.io';
 
   constructor(private  http: HttpClient) {
@@ -22,7 +21,6 @@ export class ApiService {
   getExchangeRate(currencies: Currency): Observable<ExchangeRate> {
     return this.http.get(`${this.url}/latest?base=${currencies.base}&symbols=${currencies.to}`)
       .pipe(map((data: { rates }) => {
-          console.log(data);
           return {
             base: currencies.base,
             to: currencies.to,
@@ -33,21 +31,7 @@ export class ApiService {
       );
   }
 
-  getHistoricalRate(currencies: Currency) {
-    return this.http.get(`https://api.exchangeratesapi.io/history?start_at=${(this.dateFrom)}&end_at=${(this.dateTo)}&base=${currencies.base}&symbols=${currencies.to}`).pipe(
-      map((data: { rates }) => {
-        const result: Result = {
-          name: currencies.to,
-          value: []
-        };
-        for (let date in data.rates) {
-          if (data.rates.hasOwnProperty(date)) {
-            result.value.push({
-              name: date,
-              rateValue: data.rates[date][currencies.to]
-            });
-          }
-        }
-      }))
+  getHistoricalRate(currencies: Currency): any {
+    return this.http.get(`https://api.exchangeratesapi.io/history?start_at=2020-09-10&end_at=2020-10-20&base=${currencies.base}&symbols=${currencies.to}`);
   }
 }
